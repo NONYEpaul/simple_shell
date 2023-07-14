10003d6330dc59ce1fcb26cf7efc4edf0807987d
NONYEpaul <helenonyepaul@gmail.com>
2023-07-14 13:19:09 -0700

    my main.h file

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <stderr.h>
+#include <fcntl.h>
+#include <signal.h>
+
+int main(void);
+int main(int argc, char* argv[]);
